f9eb31a8bf77ef14c8b15613df03d414db68e979
Jonathan Halfhide <jhalfhi@wgu.edu>
2023-12-12 14:48:17 -0500

    add historical and news sections, add dynamic news results

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+from datetime import datetime, timedelta
 from flask import Flask, render_template
 import requests
 import os
 
 app = Flask(__name__)
 
-# get current BTC price
+# get current BTC price for top of page
 COIN_KEY = os.environ.get("COIN_KEY")
 url = "https://rest.coinapi.io/v1/exchangerate/BTC/USD"
 headers = {
@@ -15,9 +16,26 @@ current_price = round(response.json()['rate'], 2)
 formatted_price = "{:,.2f}".format(current_price)
 
 
+# get 3 bitcoin articles from the last 30 days
+current_date = datetime.now()
+one_month_ago = current_date - timedelta(days=30)
+formatted_date = one_month_ago.strftime('%Y-%m-%d')
+NEWS_KEY = os.environ.get("NEWS_KEY")
+news_url = f"https://api.marketaux.com/v1/news/all?search=bitcoin&filter_entities=true&published_after={formatted_date}&language=en&api_token={NEWS_KEY}"
+news_request = requests.get(url=news_url)
+news_data = news_request.json()
+article_list = []
+for article in news_data['data']:
+    article_dict = {
+        'title': article['title'],
+        'description': article['description'],
+        'link': article['url']
+    }
+    article_list.append(article_dict)
+
 @app.route("/")
 def home():
-    return render_template("index.html", current_price=formatted_price)
+    return render_template("index.html", current_price=formatted_price, articles=article_list)
 
 
 """ Note: in chrome developer tools, use: Console > document.body.contentEditable=true 
